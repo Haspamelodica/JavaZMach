2d9c92cd75eeeba6ce336c452eadb1a3e2e9e66c
Haspamelodica <melodicahaspa@gmail.com>
2019-03-25 21:27:56 +0100

    Added FramedStack

---
+++ src/net/haspamelodica/javaz/FramedStack.java
@@ -0,0 +1,100 @@
+package net.haspamelodica.javaz;
+
+import java.util.Arrays;
+
+public class FramedStack
+{
+	private static final int SIZE_OVERHEAD = 10;
+
+	private short[] mem;
+
+	private int	sp;
+	private int	fp;
+
+	public FramedStack()
+	{
+		this.mem = new short[SIZE_OVERHEAD];
+	}
+
+	public int pop()
+	{
+		return getWordAt(-- sp);
+	}
+	public void push(int val)
+	{
+		setWordAt(sp ++, val);
+	}
+	public int readFPRelative(int off)
+	{
+		return getWordAt(fp + off);
+	}
+	public void writeFPRelative(int off, int val)
+	{
+		setWordAt(fp + off, val);
+	}
+	public int readAbsolute(int addr)
+	{
+		return getWordAt(addr);
+	}
+	public void writeAbsolute(int addr, int val)
+	{
+		setWordAt(addr, val);
+	}
+
+	/**
+	 * Equal to the number of words pushed onto the stack.
+	 */
+	public int getSP()
+	{
+		return sp;
+	}
+	public void setSP(int sp)
+	{
+		this.sp = sp;
+	}
+	public int getFP()
+	{
+		return fp;
+	}
+	public void setFP(int fp)
+	{
+		this.fp = fp;
+	}
+	/**
+	 * After a call to this method with argument <code>0</code> followed by <code>push(x)</code>,
+	 * <code>readFPRelative(0)</code> will return x.
+	 */
+	public void setFPSPRelative(int spOff)
+	{
+		fp = sp + spOff;
+	}
+
+	private void checkRO(int addr)
+	{
+		checkRW(addr);
+		if(addr >= sp)
+			throw new IndexOutOfBoundsException("Stack overflow");
+	}
+	private void checkRW(int addr)
+	{
+		if(addr < 0)
+			throw new IndexOutOfBoundsException("Stack underflow");
+	}
+	private int getWordAt(int i)
+	{
+		checkRO(i);
+		growMemTo(i);
+		return mem[i] & 0xFFFF;
+	}
+	private void setWordAt(int i, int val)
+	{
+		checkRW(i);
+		growMemTo(i);
+		mem[i] = (short) val;
+	}
+	private void growMemTo(int i)
+	{
+		if(mem.length < i)
+			mem = Arrays.copyOf(mem, i + SIZE_OVERHEAD);
+	}
+}
