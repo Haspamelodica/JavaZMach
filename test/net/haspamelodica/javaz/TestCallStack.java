30638d490b59fbff738f136072db69b8e4e7ac6b
Haspamelodica <melodicahaspa@gmail.com>
2019-03-27 14:13:02 +0100

    More elaborate CallStack "test"

--- test/net/haspamelodica/javaz/TestCallStack.java
+++ test/net/haspamelodica/javaz/TestCallStack.java
@@ -6,6 +6,7 @@ public class TestCallStack
 {
 	public static void main(String[] args)
 	{
+		//TODO test
 		System.out.println("initial:");
 		CallStack stack = new CallStack();
 		System.out.println(stack);
@@ -25,7 +26,13 @@ public class TestCallStack
 		stack.pop();
 		System.out.println(stack);
 		System.out.println("push call frame:");
-		stack.pushCallFrame(0x1234, 5, new int[] {1, 2, 3, 4, 5}, 2, true, 34);
+		stack.pushCallFrame(0x1234, 6, new int[] {10, 20, 30, 40, 50, 60}, 2, true, 34);
 		System.out.println(stack);
+		System.out.println(stack.getCurrentCallFrameDiscardReturnValue());
+		System.out.println(stack.getCurrentCallFrameFP());
+		System.out.println(stack.getCurrentCallFrameLocalVariableCount());
+		System.out.println(stack.getCurrentCallFrameStoreTarget());
+		System.out.println(stack.readLocalVariable(1));
+		System.out.println(stack.readLocalVariable(4));
 	}
 }
