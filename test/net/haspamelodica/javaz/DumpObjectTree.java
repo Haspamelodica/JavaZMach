9c02f1b91dd553278b745a1a51d28b400cec9411
Haspamelodica <melodicahaspa@gmail.com>
2019-03-28 23:32:03 +0100

    Wrote ugly tree printing function.
    But the dumper suggests my ObjectTree is working correctly!

--- test/net/haspamelodica/javaz/DumpObjectTree.java
+++ test/net/haspamelodica/javaz/DumpObjectTree.java
@@ -15,7 +15,42 @@ public class DumpObjectTree
 		WritableFixedSizeMemory mem = new WritableFixedSizeMemory(Files.readAllBytes(Paths.get("ZORK1.z3")));
 		ObjectTree tree = new ObjectTree(new GlobalConfig(), 3, new HeaderParser(mem), mem);
 		tree.reset();
-		System.out.println(tree.getParent(1));
-		//TODO print object tree
+		for(int objNumber = 1; objNumber < 255; objNumber ++)
+			if(tree.getParent(objNumber) == 0)
+			{
+				printObjTree(tree, objNumber);
+			}
+	}
+	private static void printObjTree(ObjectTree tree, int topLevelObjNumber)
+	{
+		boolean[] hasSiblingsStack = new boolean[255];
+		for(int objNumber = topLevelObjNumber, depth = 0; objNumber != 0;)
+		{
+			for(int i = 0; i < depth - 1; i ++)
+				System.out.print(hasSiblingsStack[i] ? " |" : "  ");
+			if(depth > 0)
+				System.out.print(" +");
+			System.out.println(objNumber);
+			int child = tree.getChild(objNumber);
+			int sibling = tree.getSibling(objNumber);
+			if(depth > 0)
+				hasSiblingsStack[depth - 1] = sibling != 0;
+			if(child != 0)
+			{
+				objNumber = child;
+				depth ++;
+			} else if(sibling != 0)
+				objNumber = sibling;
+			else
+			{
+				do
+				{
+					objNumber = tree.getParent(objNumber);
+					depth --;
+				} while(tree.getSibling(objNumber) == 0 && objNumber != 0);
+				if(objNumber != 0)
+					objNumber = tree.getSibling(objNumber);
+			}
+		}
 	}
 }
