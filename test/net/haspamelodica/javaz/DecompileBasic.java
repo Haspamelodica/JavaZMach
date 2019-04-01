1613fc294547e3c3f89aa60b874dde38db69008b
Haspamelodica <melodicahaspa@gmail.com>
2019-04-01 12:55:33 +0200

    Decompiler prints jump / branch targets

--- test/net/haspamelodica/javaz/DecompileBasic.java
+++ test/net/haspamelodica/javaz/DecompileBasic.java
@@ -8,12 +8,12 @@ import net.haspamelodica.javaz.core.HeaderParser;
 import net.haspamelodica.javaz.core.instructions.DecodedInstruction;
 import net.haspamelodica.javaz.core.instructions.InstructionDecoder;
 import net.haspamelodica.javaz.core.instructions.Opcode;
+import net.haspamelodica.javaz.core.instructions.OperandType;
 import net.haspamelodica.javaz.core.memory.SequentialMemoryAccess;
 import net.haspamelodica.javaz.core.memory.WritableFixedSizeMemory;
 import net.haspamelodica.javaz.core.memory.WritableMemory;
 import net.haspamelodica.javaz.core.text.ZCharsSeqMemUnpacker;
 import net.haspamelodica.javaz.core.text.ZCharsToZSCIIConverter;
-import net.haspamelodica.javaz.core.text.ZSCIICharStreamReceiver;
 
 public class DecompileBasic
 {
@@ -27,21 +27,26 @@ public class DecompileBasic
 		InstructionDecoder decoder = new InstructionDecoder(config, version, memSeq);
 		ZCharsToZSCIIConverter textConverter = new ZCharsToZSCIIConverter(config, version, header, mem, new ZCharsSeqMemUnpacker(memSeq));
 		textConverter.reset();
-		ZSCIICharStreamReceiver textReceiver = zsciiChar -> System.out.print((char) zsciiChar);
 		memSeq.setAddress(header.getField(HeaderParser.InitialPCLoc));
 		memSeq.setAddress(0x535E);//address of Overview example
+		memSeq.setAddress(0x8ee8);//address where ZORK print_paddr interpreter bug probably happens
 		DecodedInstruction instr = new DecodedInstruction();
 		do
 		{
 			System.out.print("pc ");
-			System.out.printf("%06x", memSeq.getAddress());
+			System.out.printf("%06x-", memSeq.getAddress());
 			decoder.decode(instr);
+			System.out.printf("%06x", memSeq.getAddress() - 1);
 			System.out.print(": ");
 			System.out.print(instr);
+			if(instr.opcode.isBranchOpcode && instr.branchOffset != 0 && instr.branchOffset != 1)
+				System.out.printf(" (branch to 0x%04x)", memSeq.getAddress() + (((instr.branchOffset - 2) << 16) >> 16));
+			if(instr.opcode == Opcode.jump && instr.operandTypes[0] != OperandType.VARIABLE && instr.operandValues[0] != 0 && instr.operandValues[0] != 1)
+				System.out.printf(" (jump to 0x%04x)", memSeq.getAddress() + (((instr.operandValues[0] - 2) << 16) >> 16));
 			if(instr.opcode.isTextOpcode)
 			{
 				System.out.print(" text: \"");
-				textConverter.decode(textReceiver);
+				textConverter.decode(zsciiChar -> System.out.print((char) zsciiChar));
 				System.out.print('"');
 			}
 			System.out.println();
