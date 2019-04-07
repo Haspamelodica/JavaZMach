c78bfb74bea99facb6a1d463b5a06f396eba55fb
Haspamelodica <melodicahaspa@gmail.com>
2019-04-08 00:23:59 +0200

    Fixed a bug deep in the core (Operand decoding)

--- src/net/haspamelodica/javaz/core/ZInterpreter.java
+++ src/net/haspamelodica/javaz/core/ZInterpreter.java
@@ -66,8 +66,7 @@ public class ZInterpreter
 
 	private final DecodedInstruction	currentInstr;
 	private final int[]					variablesInitialValuesBuf;
-	private final int[]					operandEvaluatedValuesBufUnsigned;
-	private final int[]					operandEvaluatedValuesBufSigned;
+	private final int[]					operandEvaluatedValuesBuf;
 	private final StringBuilder			stringBuf;
 
 	public ZInterpreter(GlobalConfig config, WritableMemory dynamicMem, ReadOnlyMemory mem, VideoCardDefinition vCardDef)
@@ -105,8 +104,7 @@ public class ZInterpreter
 
 		this.currentInstr = new DecodedInstruction();
 		this.variablesInitialValuesBuf = new int[16];
-		this.operandEvaluatedValuesBufUnsigned = new int[8];
-		this.operandEvaluatedValuesBufSigned = new int[8];
+		this.operandEvaluatedValuesBuf = new int[8];
 		this.stringBuf = new StringBuilder();
 	}
 
@@ -155,106 +153,106 @@ public class ZInterpreter
 
 		boolean branchCondition = false;
 
-		int o0U = operandEvaluatedValuesBufUnsigned[0];
-		int o1U = operandEvaluatedValuesBufUnsigned[1];
-		int o2U = operandEvaluatedValuesBufUnsigned[2];
-		int o0S = operandEvaluatedValuesBufSigned[0];
-		int o1S = operandEvaluatedValuesBufSigned[1];
+		int o0 = operandEvaluatedValuesBuf[0];
+		int o1 = operandEvaluatedValuesBuf[1];
+		int o2 = operandEvaluatedValuesBuf[2];
 		//Sign-extend 16 to 32 bit
-		int o0E = (o0S << 16) >> 16;
-		int o1E = (o1S << 16) >> 16;
+		int o0E = (o0 << 16) >> 16;
+		int o1E = (o1 << 16) >> 16;
 		//Opcode ordering and section numbering according to zmach06e.pdf
 		//Source: http://mirror.ifarchive.org/indexes/if-archiveXinfocomXinterpretersXspecificationXzspec02.html
 		switch(currentInstr.opcode)
 		{
 			//8.2 Reading and writing memory
 			case load:
-				storeVal = readVariable(o0U, true);
+				storeVal = readVariable(o0, true);
 				break;
 			case store:
-				writeVariable(o0U, o1U, true);
+				writeVariable(o0, o1, true);
 				break;
 			case loadw:
-				storeVal = mem.readWord(o0U + (o1U << 1));
+				storeVal = mem.readWord(o0 + (o1 << 1));
 				break;
 			case storew:
 				//TODO enforce header access rules
-				dynamicMem.writeWord(o0U + (o1U << 1), o2U);
+				dynamicMem.writeWord(o0 + (o1 << 1), o2);
 				break;
 			case loadb:
-				storeVal = mem.readByte(o0U + o1U);
+				storeVal = mem.readByte(o0 + o1);
 				break;
 			case storeb:
 				//TODO enforce header access rules
-				dynamicMem.writeByte(o0U + o1U, o2U);
+				dynamicMem.writeByte(o0 + o1, o2);
 				break;
 			case push:
-				stack.push(o0U);
+				stack.push(o0);
 				break;
 			case pull_V15:
-				writeVariable(o0U, stack.pop(), true);
+				writeVariable(o0, stack.pop(), true);
 				break;
 			//8.3 Arithmetic
 			case add:
-				storeVal = o0U + o1U;
+				storeVal = o0 + o1;
 				break;
 			case sub:
-				storeVal = o0U - o1U;
+				storeVal = o0 - o1;
 				break;
 			case mul:
-				storeVal = o0U * o1U;
+				storeVal = o0 * o1;
 				break;
 			case div:
-				if(o1S == 0 && dontIgnoreDiv0)
+				if(o1E == 0 && dontIgnoreDiv0)
 					throw new ArithmeticException("Division by 0");
-				storeVal = o0S / o1S;
+				storeVal = o0E / o1E;
 				break;
 			case mod:
-				if(o1S == 0 && dontIgnoreDiv0)
+				if(o1E == 0 && dontIgnoreDiv0)
 					throw new ArithmeticException("Division by 0");
-				storeVal = o0S % o1S;
+				storeVal = o0E % o1E;
 				break;
 			case inc:
-				writeVariable(o0U, readVariable(o0U) + 1);
+				writeVariable(o0, readVariable(o0) + 1);
 				break;
 			case dec:
-				writeVariable(o0U, readVariable(o0U) - 1);
+				writeVariable(o0, readVariable(o0) - 1);
 				break;
 			case inc_chk://inc_jg in zmach06e.pdf
-				int newVal = readVariable(o0U) + 1;
-				writeVariable(o0U, newVal);
+				int newVal = readVariable(o0) + 1;
+				writeVariable(o0, newVal);
 				branchCondition = (newVal << 16) >> 16 > o1E;
 				break;
 			case dec_chk://dec_jl in zmach06e.pdf
-				newVal = readVariable(o0U) - 1;
-				writeVariable(o0U, newVal);
+				newVal = readVariable(o0) - 1;
+				writeVariable(o0, newVal);
 				branchCondition = (newVal << 16) >> 16 < o1E;
 				break;
 			case or:
-				storeVal = o0U | o1U;
+				storeVal = o0 | o1;
 				break;
 			case and:
-				storeVal = o0U & o1U;
+				storeVal = o0 & o1;
 				break;
 			case not_V14:
 			case not_V5:
 				//TODO what for 8 bit arguments?
-				storeVal = ~o0U;
+				storeVal = ~o0;
 				break;
 			case art_shift:
-				storeVal = o1E < 0 ? o0S >> -o1E : o0U << o1E;
+				//TODO signed or unsigned?
+				storeVal = o1E < 0 ? o0E >> -o1E : o0 << o1E;
 				break;
 			case log_shift:
-				storeVal = o1E < 0 ? o0U >>> -o1E : o0U << o1E;
+				//TODO signed or unsigned?
+				storeVal = o1E < 0 ? o0 >>> -o1E : o0 << o1E;
 				break;
 			//8.4 Comparison and jumps
 			case jz:
-				branchCondition = o0U == 0;
+				branchCondition = o0 == 0;
 				break;
 			case je:
 				branchCondition = false;
 				for(int i = 1; i < currentInstr.operandCount; i ++)
-					if(o0U == operandEvaluatedValuesBufUnsigned[i])
+					if(o0 == operandEvaluatedValuesBuf[i])
 					{
 						branchCondition = true;
 						break;
@@ -267,15 +265,15 @@ public class ZInterpreter
 				branchCondition = o0E > o1E;
 				break;
 			case jin:
-				branchCondition = objectTree.getParent(o0U) == o1U;
+				branchCondition = objectTree.getParent(o0) == o1;
 				break;
 			case test:
-				branchCondition = (o0U & o1U) == o1U;
+				branchCondition = (o0 & o1) == o1;
 				break;
 			case jump:
 				//Sign-extend 16 to 32 bit
 				//TODO signed or unsigned?
-				memAtPC.skipBytes(((o0S - 2) << 16) >> 16);
+				memAtPC.skipBytes(((o0E - 2) << 16) >> 16);
 				break;
 			//8.5 Call and return, throw and catch
 			case call_1s://call_f0 in zmach06e.pdf
@@ -290,10 +288,10 @@ public class ZInterpreter
 				doStore = false;//return will do this store
 				int argCount = currentInstr.operandCount - 1;
 				boolean discardRetVal = !currentInstr.opcode.isStoreOpcode;
-				doCallTo(o0U, argCount, operandEvaluatedValuesBufUnsigned, 1, discardRetVal, currentInstr.storeTarget, true);
+				doCallTo(o0, argCount, operandEvaluatedValuesBuf, 1, discardRetVal, currentInstr.storeTarget, true);
 				break;
 			case ret:
-				doReturn(o0U);
+				doReturn(o0);
 				break;
 			case rtrue:
 				doReturn(1);
@@ -305,64 +303,66 @@ public class ZInterpreter
 				doReturn(readVariable(0));
 				break;
 			case check_arg_count:
-				branchCondition = stack.getCurrentCallFrameSuppliedArgumentsCount() >= o0U;
+				branchCondition = stack.getCurrentCallFrameSuppliedArgumentsCount() >= o0;
 				break;
 			//8.6 Objects, attributes, and properties
 			case get_sibling:
-				storeVal = objectTree.getSibling(o0U);
+				storeVal = objectTree.getSibling(o0);
 				branchCondition = storeVal != 0;
 				break;
 			case get_child:
-				storeVal = objectTree.getChild(o0U);
+				storeVal = objectTree.getChild(o0);
 				branchCondition = storeVal != 0;
 				break;
 			case get_parent:
-				storeVal = objectTree.getParent(o0U);
+				storeVal = objectTree.getParent(o0);
 				break;
 			case remove_obj:
-				objectTree.removeObj(o0U);
+				objectTree.removeObj(o0);
 				break;
 			case insert_obj:
-				objectTree.insertObj(o0U, o1U);
+				objectTree.insertObj(o0, o1);
 				break;
 			case test_attr:
-				branchCondition = objectTree.getAttribute(o0U, o1U) == 1;
+				branchCondition = objectTree.getAttribute(o0, o1) == 1;
 				break;
 			case set_attr:
-				objectTree.setAttribute(o0U, o1U, 1);
+				objectTree.setAttribute(o0, o1, 1);
 				break;
 			case clear_attr:
-				objectTree.setAttribute(o0U, o1U, 0);
+				objectTree.setAttribute(o0, o1, 0);
 				break;
 			case put_prop:
-				objectTree.putPropOrThrow(o0U, o1U, o2U);
+				objectTree.putPropOrThrow(o0, o1, o2);
 				break;
 			case get_prop:
-				storeVal = objectTree.getPropOrDefault(o0U, o1U);
+				storeVal = objectTree.getPropOrDefault(o0, o1);
 				break;
 			case get_prop_addr:
-				storeVal = objectTree.getPropAddr(o0U, o1U);
+				storeVal = objectTree.getPropAddr(o0, o1);
 				break;
 			case get_next_prop:
-				storeVal = objectTree.getNextProp(o0U, o1U);
+				storeVal = objectTree.getNextProp(o0, o1);
 				break;
 			case get_prop_len:
-				storeVal = objectTree.getPropSize(o0U);
+				storeVal = objectTree.getPropSize(o0);
 				break;
 			//8.7 Windows
 			//8.8 Input and output streams
+			//8.9 Input
+			case aread://read in zmach06e.pdf
 			case sread://read in zmach06e.pdf
+				//TODO show status bar
 				//TODO timeouts
-				wBuf.reset(o0U, version < 5, 1);
+				wBuf.reset(o0, version < 5, 1);
 				storeVal = ioCard.inputToTextBuffer(wBuf);
-				rBuf.reset(o0U, version < 5, 1);
-				wBuf.reset(o1U, false, 4);
+				rBuf.reset(o0, version < 5, 1);
+				wBuf.reset(o1, false, 4);
 				tokeniser.tokenise(rBuf, wBuf);
 				break;
-			//8.9 Input
 			//8.10 Character based output
 			case print_char:
-				ioCard.printZSCII(o0U);
+				ioCard.printZSCII(o0);
 				break;
 			case new_line:
 				ioCard.printZSCII(13);
@@ -376,22 +376,22 @@ public class ZInterpreter
 				doReturn(1);
 				break;
 			case print_addr:
-				seqMemROBuf.setAddress(o0U);
+				seqMemROBuf.setAddress(o0);
 				textConvFromSeqMemROBuf.decode(printZSCIITarget);
 				break;
 			case print_paddr:
-				seqMemROBuf.setAddress(packedToByteAddr(o0U, false));
+				seqMemROBuf.setAddress(packedToByteAddr(o0, false));
 				textConvFromSeqMemROBuf.decode(printZSCIITarget);
 				break;
 			case print_num:
 				//Sign-extend 16 to 32 bit
-				stringBuf.append((o0S << 16) >> 16);
+				stringBuf.append((o0E << 16) >> 16);
 				for(int i = 0; i < stringBuf.length(); i ++)
 					ioCard.printZSCII(stringBuf.charAt(i));
 				stringBuf.setLength(0);
 				break;
 			case print_obj:
-				seqMemROBuf.setAddress(objectTree.getObjectNameLoc(o0U));
+				seqMemROBuf.setAddress(objectTree.getObjectNameLoc(o0));
 				textConvFromSeqMemROBuf.decode(printZSCIITarget);
 				break;
 			//8.11 Miscellaneous screen output
@@ -400,11 +400,11 @@ public class ZInterpreter
 			//8.14 Miscellaneous
 			case random:
 				if(o0E > 0)
-					storeVal = rand.nextInt(o0U);
+					storeVal = rand.nextInt(o0);
 				else
 				{
 					storeVal = 0;
-					if(o0U == 0)
+					if(o0 == 0)
 						rand.setSeed(trueRandom.nextLong());
 					else
 						rand.setSeed(-o0E);
@@ -450,19 +450,14 @@ public class ZInterpreter
 		switch(instr.operandTypes[i])
 		{
 			case LARGE_CONST:
-				operandEvaluatedValuesBufSigned[i] = specifiedVal;
-				operandEvaluatedValuesBufUnsigned[i] = specifiedVal;
+				operandEvaluatedValuesBuf[i] = specifiedVal;
 				break;
 			case SMALL_CONST:
-				//Sign-extend 8 to 16 bit
-				operandEvaluatedValuesBufSigned[i] = ((specifiedVal << 24) >> 24) & 0xFFFF;
-				operandEvaluatedValuesBufUnsigned[i] = specifiedVal;
+				operandEvaluatedValuesBuf[i] = specifiedVal;
+				//operandEvaluatedValuesBuf[i] = ((specifiedVal << 24) >> 24)&0xFFFF;
 				break;
 			case VARIABLE:
-				int valU = readVariable(specifiedVal);
-				//Sign-extend 16 to 32 bit
-				operandEvaluatedValuesBufSigned[i] = (valU << 16) >> 16;
-				operandEvaluatedValuesBufUnsigned[i] = valU;
+				operandEvaluatedValuesBuf[i] = readVariable(specifiedVal);
 				break;
 			default:
 				throw new IllegalArgumentException("Unknown enum type: " + instr.operandTypes[i]);
