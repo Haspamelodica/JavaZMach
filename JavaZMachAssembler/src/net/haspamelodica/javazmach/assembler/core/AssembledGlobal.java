9b4a22fb71196b35066806c2a478b7dbc90f4c9e
Haspamelodica <melodicahaspa@gmail.com>
2024-01-11 00:21:31 +0100

    Adjusted AssembledGlobal to use ResolvableIntegralValue

--- JavaZMachAssembler/src/net/haspamelodica/javazmach/assembler/core/AssembledGlobal.java
+++ JavaZMachAssembler/src/net/haspamelodica/javazmach/assembler/core/AssembledGlobal.java
@@ -1,40 +1,28 @@
 package net.haspamelodica.javazmach.assembler.core;
 
+import static net.haspamelodica.javazmach.assembler.core.AssemblerIntegralValue.intConst;
 import static net.haspamelodica.javazmach.assembler.core.ZAssemblerUtils.bigintIntChecked;
 
-import java.math.BigInteger;
-import java.util.Optional;
-
 import net.haspamelodica.javazmach.assembler.model.Global;
-import net.haspamelodica.javazmach.assembler.model.IntegralValue;
 import net.haspamelodica.javazmach.core.memory.SequentialMemoryWriteAccess;
 
 public class AssembledGlobal
 {
-	private final Optional<IntegralValue>	defaultValue;
-	private BigInteger						defaultValueResolved;
+	private final ResolvableIntegralValue defaultValue;
 
 	public AssembledGlobal(Global global)
 	{
-		this.defaultValue = global.initialValue();
-		this.defaultValueResolved = null;
+		this.defaultValue = new ResolvableIntegralValue(global.initialValue().map(AssemblerIntegralValue::intVal).orElse(intConst(0)));
 	}
 
 	public void updateResolvedValues(LocationResolver locationResolver)
 	{
-		defaultValueResolved = defaultValue.map(v -> ZAssemblerUtils.integralValueOrNull(v, locationResolver)).orElseGet(() -> BigInteger.valueOf(0));
+		defaultValue.updateResolvedValue(locationResolver);
 	}
 
 	public void append(SpecialLocationEmitter locationEmitter, SequentialMemoryWriteAccess memSeq, DiagnosticHandler diagnosticHandler)
 	{
-		if(defaultValueResolved == null)
-		{
-			diagnosticHandler.error("Failed to resolve initial value for global");
-			memSeq.writeNextWord(0);
-		} else
-		{
-			memSeq.writeNextWord(bigintIntChecked(16, defaultValueResolved,
-					(b) -> "Default value %s too large for global. Must be at most 2 bytes".formatted(b), diagnosticHandler));
-		}
+		memSeq.writeNextWord(bigintIntChecked(16, defaultValue.resolvedValueOrZero(),
+				(b) -> "Default value %s too large for global. Must be at most 2 bytes".formatted(b), diagnosticHandler));
 	}
 }
