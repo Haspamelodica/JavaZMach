a966b8e8e3114d02aedbbbf0ad3818833bee79a7
Haspamelodica <melodicahaspa@gmail.com>
2024-01-21 03:54:18 +0100

    Added macro for adding 64-bit constants to 4 variables

--- JavaZMachAssembler/src/TestEmulatedCarry.java
+++ JavaZMachAssembler/src/TestEmulatedCarry.java
@@ -125,61 +125,55 @@ public class TestEmulatedCarry
 		// "if(aN_carry)" is the jump target,
 		// and all code between the trigger and the target is guarded with "if(!aN_carry)".
 		// Also, there will be an implicit jump from just before the "if(aN_carry)" to after it.
-		boolean a_1_carry = false;
 		boolean a0_carry = false;
 		boolean a1_carry = false;
 		boolean a2_carry = false;
 		boolean a3_carry = false;
+		boolean a0_justcheck = false;
+		boolean a1_justcheck = false;
+		boolean a2_justcheck = false;
+		boolean a3_justcheck = false;
 
-		if(!a_1_carry && !a0_carry)
+		if(!a0_justcheck && !a0_carry)
 			a0 += b0 + 0x80;
 		if(!a0_carry)
 			a0_carry = a0 < (byte) (b0 + 0x80);
 		if(a0_carry)
 		{
 			// there was a carry
+			a1 += b1 + 0x81;
 			if(b1 == (byte) 0xff)
-			{
-				a1 += 0x80;
 				a1_carry = true;
-			}
-			if(!a1_carry)
-				a1 += b1 + 0x81;
+			a1_justcheck = true;
 		}
 
-		if(!a0_carry && !a1_carry)
+		if(!a1_justcheck && !a1_carry)
 			a1 += b1 + 0x80;
 		if(!a1_carry)
 			a1_carry = a1 < (byte) (b1 + 0x80);
 		if(a1_carry)
 		{
 			// there was a carry
+			a2 += b2 + 0x81;
 			if(b2 == (byte) 0xff)
-			{
-				a2 += 0x80;
 				a2_carry = true;
-			}
-			if(!a2_carry)
-				a2 += b2 + 0x81;
+			a2_justcheck = true;
 		}
 
-		if(!a1_carry && !a2_carry)
+		if(!a2_justcheck && !a2_carry)
 			a2 += b2 + 0x80;
 		if(!a2_carry)
 			a2_carry = a2 < (byte) (b2 + 0x80);
 		if(a2_carry)
 		{
 			// there was a carry
+			a3 += b3 + 0x81;
 			if(b3 == (byte) 0xff)
-			{
-				a3 += 0x80;
 				a3_carry = true;
-			}
-			if(!a3_carry)
-				a3 += b3 + 0x81;
+			a3_justcheck = true;
 		}
 
-		if(!a2_carry && !a3_carry)
+		if(!a3_justcheck && !a3_carry)
 			a3 += b3 + 0x80;
 		if(!a3_carry)
 			a3_carry = a3 < (byte) (b3 + 0x80);
